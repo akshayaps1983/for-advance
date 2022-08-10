99d35b5386dff0f7dbc802c4144146365a9f2350
akshayaps1983 <110896904+akshayaps1983@users.noreply.github.com>
2022-08-10 14:14:35 +0530

    first

---
+++ list1.py
@@ -0,0 +1,10 @@
+newlist=[1,2,3,4,5,6]
+print(newlist)
+newlist.clear()
+print(newlist)
+thislist=["Apple","banana","cherry","jackfruite"]
+print(thislist)
+print(thislist.sort())
+print(thislist.sort(reverse=True))
+thislist.sort(reverse=True)
+print(thislist)
